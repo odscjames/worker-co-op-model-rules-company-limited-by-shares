b30fa508a619fe2a0ff0d80fd734201d3efa508a
James Baster <james.baster@opendataservices.coop>
2018-10-10 12:47:21 +0100

    Next bit is difficult bit, but I'm changing trains ...

--- web/legal-xml-parser.js
+++ web/legal-xml-parser.js
@@ -15,13 +15,43 @@ class LegalXMLParser {
         for (var i = 0; i < oDOM.firstChild.childNodes.length; i++) {
           var node = oDOM.firstChild.childNodes[i];
           if(node.nodeType !== Node.TEXT_NODE) {
-            console.log(node);
+            if(node.localName == 'title') {
+                this._process_title(node);
+            } else if (node.localName == 'subtitle') {
+                this._process_subtitle(node);
+            } else if (node.localName == 'body') {
+                this._process_body(node);
+            }
           }
         }
 
 
 
 
+
+    }
+
+    _process_title(node) {
+    }
+
+    _process_subtitle(node) {
+    }
+
+    _process_body(node) {
+
+
+
+        for (var i = 0; i < node.childNodes.length; i++) {
+          var childNode = node.childNodes[i];
+          if(childNode.nodeType !== Node.TEXT_NODE) {
+            this._process_body_node(childNode);
+          }
+        }
+
+    }
+
+    _process_body_node(node) {
+        console.log(node)
     }
 
     get() {
