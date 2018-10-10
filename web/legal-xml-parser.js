fb46a454ced74b10830262ab9ac5e3dc81c2888d
James Baster <james.baster@opendataservices.coop>
2018-10-10 12:41:34 +0100

    doing this on a train is painful

--- web/legal-xml-parser.js
+++ web/legal-xml-parser.js
@@ -3,12 +3,29 @@
 
 class LegalXMLParser {
     constructor(data) {
-        this.data = data
+        this.data = data;
+
+        this.out_title = "";
+        this.out_subtitle = "";
+        this.out_body = "";
+
+        var oParser = new DOMParser();
+        var oDOM = oParser.parseFromString(data, "application/xml");
+
+        for (var i = 0; i < oDOM.firstChild.childNodes.length; i++) {
+          var node = oDOM.firstChild.childNodes[i];
+          if(node.nodeType !== Node.TEXT_NODE) {
+            console.log(node);
+          }
+        }
+
+
+
+
     }
 
     get() {
 
-    
 
 
 
